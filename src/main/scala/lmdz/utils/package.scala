5442fe88ac670f746dc3fa496cae4268f9be636e
sidnt <sidnt@users.noreply.github.com>
2019-12-12 07:52:17 +0530

    add array<->buffer interconversion & console utils

---
+++ src/main/scala/lmdz/utils/package.scala
@@ -0,0 +1,37 @@
+package lmdz
+
+import java.io.IOException
+
+import zio._
+import zio.console._
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets.UTF_8
+
+package object utils {
+
+  /** we need the ability of interconversion between
+   * runtime objects <-> database objects */
+
+  /**where will be dbb allocated? in the method's stack?
+   * will it need GC later? Or will it be wiped after this method returns? */
+  def byteArrayToDByteBuffer(ba: Array[Byte]): ByteBuffer = {
+    val dbb: ByteBuffer = ByteBuffer.allocateDirect(ba.length).put(ba)
+    dbb.flip()
+    dbb
+    /** is ↑ concurrent safe?
+     * seems so, because it doesn't access any value outside itself
+     * even if it mutates something `dbb.flip()` the values affected are local to the method
+     * each executing instance will have its own copy
+     * and won't be working with any shared state. so looks like fiber safe? */
+  }
+
+  /** these apis are #hardcoded to the UTF_8 Charset */
+  def stringToUtf8DByteBuffer(s:String): ByteBuffer = byteArrayToDByteBuffer(s.getBytes(UTF_8))
+  def dByteBufferToUtf8String(bb:ByteBuffer): String = UTF_8.decode(bb).toString
+
+  /** console conveniences */
+  val gsl: ZIO[Console, IOException, String] = getStrLn
+  val psl: String => URIO[Console, Unit] = (s:String)  =>  putStrLn(s)
+  val ps: String => URIO[Console, Unit] = (s:String)  =>  putStr(s)
+
+}
