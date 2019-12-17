8881f5d7df3748d58087a9a9309916115725a640
sidnt <sidnt@users.noreply.github.com>
2019-12-17 18:25:54 +0530

    refactor createOrOpenDefaultEnvAndDbi → openDefaultEnvAndDbi

--- lmdzclient0/src/main/scala/lmdzclient0/package.scala
+++ lmdzclient0/src/main/scala/lmdzclient0/package.scala
@@ -1,14 +1,25 @@
 import java.nio.ByteBuffer
 
 import lmdz.apis._
+import lmdz.utils._
 import lmdz.cfgs.LmdzBatteryPack
-import org.lmdbjava.{Dbi, Env}
 import zio._
+import zio.console.Console
 
 package object lmdzclient0 {
 
-  val app0: RIO[LmdzBatteryPack, (Env[ByteBuffer], Dbi[ByteBuffer])] = for {
-    (env,dbi)     <-      createOrOpenDefaultEnvAndDbi
-  } yield (env, dbi)
+  val app0: ZIO[Console with LmdzBatteryPack, Throwable, Unit] = for {
+    (env,dbi)     <-      openDefaultEnvAndDbi
+    key           <-      psl("key? ") *> gsl
+    vlu           <-      psl("value? ") *> gsl
+    kbb           <-      IO.effect(stringToUtf8DByteBuffer(key))
+    vbb           <-      IO.effect(stringToUtf8DByteBuffer(vlu))
+    _             <-      crud.put(vbb,dbi,kbb)
+    key2          <-      psl("key? ") *> gsl
+    kbb2          <-      IO.effect(stringToUtf8DByteBuffer(key2))
+    vbb2          <-      crud.get(kbb2,dbi,env.txnRead())
+    vlu2          <-      IO.effect(dByteBufferToUtf8String(vbb2))
+    _             <-      psl(vlu2)
+  } yield ()
 
 }
