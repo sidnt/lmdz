dc4c25b18f00d2469b068ae0f2025fc5e84fd9d7
sidnt <sidnt@users.noreply.github.com>
2019-12-17 18:25:54 +0530

    refactor createOrOpenDefaultEnvAndDbi → openDefaultEnvAndDbi

--- src/main/scala/lmdz/apis/package.scala
+++ src/main/scala/lmdz/apis/package.scala
@@ -11,16 +11,23 @@ import LmdzBatteryPackAccessors._
 
 package object apis {
 
-  /** lmdbjava creates the environment directory if it's not there
-   * this api returns a ready to use lmdb-env & lmdb-dbi ootb */
-  val createOrOpenDefaultEnvAndDbi: RIO[LmdzBatteryPack, (Env[ByteBuffer], Dbi[ByteBuffer])] = for {
+  /** lmdbjava doesn't create the environment directory if it's not there
+   * this effect will fail if the default directory path isn't already there #unsafe
+   * this api returns a ready to use lmdb-env & lmdb-dbi ootb
+   * .
+   * #tip calling this effect again & again to get (env,dbi) at multiple places
+   * isn't a good idea because `Env.open` & `env.openDbi` will be run again
+   * which is redundant computation within the scope of a running lmdz
+   * (env,dbi) references must be shared/reused if these defaults are to be reused
+   * via concurrent accesses handled properly */
+  val openDefaultEnvAndDbi: RIO[LmdzBatteryPack, (Env[ByteBuffer], Dbi[ByteBuffer])] = for {
     defaultEnvPath  <-  DefaultEnvPath
     defaultEnvSize  <-  DefaultEnvMiBs
     defaultEnvFlag  <-  DefaultEnvFlag
     defaultDbiName  <-  DefaultDbiName
     defaultDbiFlag  <-  DefaultDbiFlag
     env             <-  IO.effect(Env.open(new File(defaultEnvPath), defaultEnvSize, defaultEnvFlag))
-    dbi             <-  IO.effect(env.openDbi(defaultDbiName,defaultDbiFlag))
+    dbi             <-  IO.effect(env.openDbi(defaultDbiName, defaultDbiFlag))
   } yield (env,dbi)
 
 }
