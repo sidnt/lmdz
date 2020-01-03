1a27ee81f6fde39a56f9dc6e3aafe860c740ca8c
sidnt <sidnt@users.noreply.github.com>
2020-01-03 21:30:37 +0530

    fix error

--- lmdzclient/src/main/scala/lmdzclient/main.scala
+++ lmdzclient/src/main/scala/lmdzclient/main.scala
@@ -7,7 +7,7 @@ import lmdz.cfgs.LmdzBatteryPack.DefaultBatteryPack
 object main extends App {
 
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
-    app1.
+    app0.
       provide(new Console.Live with DefaultBatteryPack).
       fold( (f: Any) => {println(f.toString);1}, (s:Any) => {println(s.toString);0})
 
