c39ea8660f6a493ad4a704dd3068f69b95a192e7
sidnt <sidnt@users.noreply.github.com>
2020-01-21 21:00:13 +0530

    depend on zioNio

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,11 @@ object Dependencies {
   object Versions {
     val zioV = "1.0.0-RC17"
     val lmdb4jV = "0.7.0"
+    val zioNioV = "1.0.0-RC2"
   }; import Versions._
 
   lazy val zio = "dev.zio" %% "zio" % zioV
   lazy val lmdb4j = "org.lmdbjava" % "lmdbjava" % lmdb4jV
+  lazy val zioNio = "dev.zio" %% "zio-nio" % zioNioV
 
 }
